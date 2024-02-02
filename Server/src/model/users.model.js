6bd0e9832ee0e842e83280be950c0e14b65eb7e5
Rajesh <rajeshcode07@gmail.com>
2024-02-02 15:46:42 +0530

    fixed the user Schema bug

--- Server/src/model/users.model.js
+++ Server/src/model/users.model.js
@@ -4,17 +4,15 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI);
 
 const UserSchema = new mongoose.Schema({
-  user: {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: [true, 'password is mandatory']
   }
 }, { timestamps: true })
 
